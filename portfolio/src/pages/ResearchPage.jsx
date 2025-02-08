c9a7e81ba63cbd817d85e04119d78959d79b6b3c
Tofulati <almtho2003@gmail.com>
2025-02-08 01:05:14 -0800

    update research page

--- portfolio/src/pages/ResearchPage.jsx
+++ portfolio/src/pages/ResearchPage.jsx
@@ -1,31 +1,33 @@
-import React from "react";
-// import gep from "../assets/Research/"
-// import llnl from "../assets/Research/"
-// import belieze from "../assets/Research/"
-import temp from "../assets/temp.png"
+import React, { useState } from "react";
 import "../styles/ResearchPage.css";
 
 function ResearchPage() {
+    const [openDropdown, setOpenDropdown] = useState(null);
+
+    const toggleDropdown = (index) => {
+        setOpenDropdown(openDropdown === index ? null : index);
+    };
+
     const researchs = [
         {
             title: "Multi-omic profiling of age-specific human hematopoiesis",
             timeline: "October 2024 - Present",
             description: "Analyzed scRNA-sequencing algorithms (*CAT) using stem cell data. Data analysis revealed the algorithm could be used to reverse-engineer data originally found through consensus non-negative matrix factorization.",
-            imageUrl: temp,
+            pdfLink: "https://github.com/Tofulati/cd34hult",
         },
         {
-            title: "Lawerence Livermore National Laboratory Data Science Challenge",
+            title: "Lawrence Livermore National Laboratory Data Science Challenge",
             timeline: "August 2024 - October 2024",
             description: "During this two-week challenge, we were tasked with classifying/predicting heart ECG data from Kaggle and UCSD datasets. We developed and optimized machine learning models and classifiers utilizing scikit-learn, pandas, numpy, and pytorch. Finally, we presented a poster to peers and professionals in the field.",
-            imageUrl: temp,
+            pdfLink: "../assets/project/Final_Poster.pdf",
         },
         {
             title: "Exploring the Bird Tower Cave in 3D",
             timeline: "April 2023",
             description: "University of California, Merced Bobcat Day Anthropology LibraryCave Project. This project aimed to create a virtual visualization of the Belize Bird Tower. This visualization would allow users to explore the cave's scan while immersed in a 180-degree display.",
-            imageUrl: temp,
+            pdfLink: "../assets/project/Poster1_Exploring the Bird Tower Cave in 3D.pdf",
         },
-    ]
+    ];
 
     return (
         <section id="research" className="research">
@@ -34,16 +36,28 @@ function ResearchPage() {
                 <div className="research-grid">
                     {researchs.map((research, index) => (
                         <div key={index} className="research-card">
-                            <img src={research.imageUrl} alt={research.title} />
                             <h3>{research.title}</h3>
                             <p>{research.timeline}</p>
                             <p>{research.description}</p>
+                            <button
+                                className="dropdown-button"
+                                onClick={() => toggleDropdown(index)}
+                            >
+                                Project Resources 
+                            </button>
+                            {openDropdown === index && (
+                                <div className="dropdown-content">
+                                    <a href={research.pdfLink} target="_blank" download>
+                                        Download File
+                                    </a>
+                                </div>
+                            )}
                         </div>
                     ))}
                 </div>
             </div>
         </section>
-    )
+    );
 }
 
 export default ResearchPage;
